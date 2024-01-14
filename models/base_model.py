4f4f43307a26ed93efbfc12955cf89001fd1ce3e
Babuhatarii <ochegoharyon@gmail.com>
2024-01-14 13:57:26 -0800

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@ from which all other classes will inherit thier properties"""
 
 import uuid
 from datetime import datetime
-import models
+from models import storage
 
 
 class BaseModel():
@@ -43,7 +43,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+            storage.new(self)
 
     def __str__(self):
         """String representation of the objects of this class"""
